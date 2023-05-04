2fbd623b96a98c34205f1961d3fd4d813effbadd
KislovKA <Kirill.kislov@akbarsdigital.ru>
2023-05-04 17:27:48 +0300

    Правка паттерна стратегия

--- IISemetsr11207/DesignPatternsGof/Behavioral/Strategy/CityDuck.cs
+++ IISemetsr11207/DesignPatternsGof/Behavioral/Strategy/CityDuck.cs
@@ -2,13 +2,4 @@
 
 public class CityDuck : Duck
 {
-    public override void Quack()
-    {
-        Console.WriteLine("Я городская утка, давай сюда свой хлебушек - кря!");
-    }
-
-    public override void Fly()
-    {
-        Console.WriteLine("Я улетел за латте! ");
-    }
 }
--- IISemetsr11207/DesignPatternsGof/Behavioral/Strategy/IFlyBehavior.cs
+++ IISemetsr11207/DesignPatternsGof/Behavioral/Strategy/IFlyBehavior.cs
@@ -2,5 +2,5 @@
 
 public interface IFlyBehavior
 {
-    
+    void Fly();
 }
