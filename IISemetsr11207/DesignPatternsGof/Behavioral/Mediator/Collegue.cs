6a69056874aab5ea64ff1e7ed7d3598b16ce7a6e
Kislov Kirill <kislov-k@inbox.ru>
2023-05-12 22:02:03 +0300

    Добавлен паттерн медиатор

---
+++ IISemetsr11207/DesignPatternsGof/Behavioral/Mediator/Collegue.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatternsGof.Behavioral.Mediator;
+
+public abstract class Collegue<T>
+{
+    protected IMediator<T> _mediator;
+
+    protected Collegue(IMediator<T> mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public abstract void Notify(string message);
+}
