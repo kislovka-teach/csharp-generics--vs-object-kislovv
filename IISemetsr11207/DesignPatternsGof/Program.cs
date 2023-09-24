e3d252b1f79b8ce28f6f92e92f2343f94991ec3c
Kislov Kirill Alexseevich <kislov-k@inbox.ru>
2023-09-24 10:30:27 +0300

    правка mr

--- IISemetsr11207/DesignPatternsGof/Program.cs
+++ IISemetsr11207/DesignPatternsGof/Program.cs
@@ -16,55 +16,98 @@ var consoleWithFileLogger = ILogger.CreateBuilder()
 
 Console.WriteLine("Hello, World!");
 */
-#endregion
+//var product = new Product(new GetProductIdeaState());
 
-#region State
-/*
-var product = new Product();
+//product.UseProduct();
 
-product.UseProduct();
+//product.UpdateProduct();
 
-product.UpdateProduct();
+//product.UseProduct();
 
-product.UseProduct();
+//product.CreateProduct();
 
-product.CreateProduct();
+//product.UseProduct();
 
-product.UseProduct();
-*/
-#endregion
-
-#region Abstract Factory
-
-var elf = new Hero(new ElfFactory());
-elf.Hit();
-elf.Move();
-
-#endregion
-
-
-static string SecondProblem(int[] numbers)
+int MaxProfit(int[] prices)
 {
-    var strings = new string[numbers.Length];
-    for (var i = 0; i < numbers.Length; i++)
+    var profit = 0;
+    for (int day = 0; day < prices.Length - 1; day++)
     {
-        strings[i] = numbers[i].ToString();
+        // always buy stock because we can sell it the same day for 0 loss
+        profit -= prices[day];
+        if (prices[day] > prices[day + 1]) profit += prices[day]; // sell stock same day
+        else profit += prices[day + 1]; // else sell stock "tomorrow" for a profit
     }
+    return profit;
+}
 
-    for (int i = 0; i < strings.Length; i++)
+int[] PlusOne(int[] digits)
+{
+    var hasShift = false;
+    var index = digits.Length - 2;
+
+    if (digits[^1] == 9)
     {
-        for (int j = 0; j < strings.Length - 1; j++)
+        digits[^1] = 0;
+        hasShift = true;
+    }
+    do
+    {
+        if (hasShift && digits[index] == 9)
         {
-            if ((strings[j] + strings[j + 1]).CompareTo(strings[j + 1] + strings[j]) < 0)
-            {
-                (strings[j], strings[j + 1]) = (strings[j + 1], strings[j]);
-            }
+            digits[index] = 0;
+            hasShift = true;
+            index--;
+        }
+        else
+        {
+            digits[index]++;
+            hasShift = false;
+            break;
+        }
+
+    }
+    while (index > 0);
+
+    if (!hasShift) return digits;
+
+    var copy = new int[digits.Length + 1];
+    Array.Copy(digits, 0, copy, 1, digits.Length);
+    copy[0] = 1;
+    return copy;
+
+}
+
+int MaxProfitLocal(int[] prices, int low, int high)
+{
+    var innerProfit = 0;
+    var n = high - low;
+    switch (n)
+    {
+        case < 1:
+            return innerProfit;
+        case > 1:
+        {
+            var mid = low + n / 2;
+            innerProfit = MaxProfitLocal(prices, low, mid) + MaxProfitLocal(prices, mid, high);
+            break;
         }
     }
 
-    return string.Join("", strings);
+    var profit = 0;
+    var min = prices[low];
+    for (var i = low; i <= high; i++)
+    {
+        if (prices[i] < min)
+        {
+            min = prices[i];
+        }
+        else
+        {
+            profit = prices[i] - min > profit ? prices[i] - min : profit;
+        }
+    }
+    return profit > innerProfit ? profit : innerProfit;
 }
 
-var numbers = new int[] { 63, 2, 4, 0, 95, 199, 101 };
-var nums = new int[] { 3, 30, 34, 5, 9 };
-Console.WriteLine(SecondProblem(nums));
+var maxProfit = MaxProfit(new []{7, 1, 5, 3, 6, 4});
